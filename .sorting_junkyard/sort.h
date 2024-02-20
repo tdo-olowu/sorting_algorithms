37c8ee0ec65913bb7a79edc0f1bb593b6723c728
Tayo Olowu <olowu.otayo@gmail.com>
2024-02-20 13:02:29 +0100

    files updated

--- .sorting_junkyard/sort.h
+++ .sorting_junkyard/sort.h
@@ -15,20 +15,40 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
+
+/**
+ * struct ordering_t - binds names and comparators
+ * @name: the name of the comparator e.g. "geq"
+ * @comparator: the function which compares numbers
+ *
+ * Description
+ * sorting algorithms may use different ways of comparing
+ * elements (e.g. is A(i) <= A(i+1)? is A(i) > A(i+i)?, etc.
+ * This data structure is for the sake of generality.
+ * By default the <= relation will be used.
+ */
+typedef struct ordering_s
+{
+	char *name;
+	int (*comparator)(int, int);
+} ordering_t;
+
+
+/* COMPARATORS */
+int leq_ord(int, int);
+int geq_ord(int, int);
+int eq_ord(int, int);
+int div_ord(int, int);
 
 
 /* PRINTING FUNCTIONS */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-
-/** SIMPLE SUBROUTINES */
-void swap(int *a, int *b);
 
 
 /* SORTING FUNCTIONS */
@@ -49,4 +69,4 @@ void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
 
-#endif /* SORT_H */
+#endif SORT_H /* SORT_H */
--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,52 @@
-#include <sort.h>
+#include "sort.h"
+
+
+
+/**
+ * bubble_sort - sorts an array acc/ to bubble sort
+ * aka sinking sort. in the code, ssb is the length of
+ * the sorted sub-array to the right end.
+ * ssb increments with each pass of the bubble_sort
+ *
+ * @array: a pointer to an array of integers
+ * @size: the number of items in the array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t lvl, j, ssb;
+
+	if (!array)
+		return;
+	if (size <= 1)
+		return;
+
+	for (lvl = 0, ssb = 0; lvl < size - 1 ; ++lvl, ++ssb)
+	{
+		for (j = 0 ; j < size - 1 - ssb ; ++j)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+
+
+/**
+ * swap - exchanges the values of two positions in array
+ * @a: the address of the first position
+ * @b: the address of the second position
+ * Return: nothing.
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
